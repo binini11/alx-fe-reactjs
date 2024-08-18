f170c6b1941c5174eb2283a932f5c303dae8390c
Bino <you@example.comclear>
2024-08-18 18:07:55 +0300

    text alingened to center in header

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <header
-      style={{ backgroundColor: "navy", color: "white", textlign: "center" }}
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
     >
-      <h1>Favorite Cities</h1>
+      <h1 style={{ textAligh: "center" }}>Favorite Cities</h1>
     </header>
   );
 }
