56c40f37631e63822878640978854f7ba35602f9
Bino <you@example.comclear>
2024-08-18 21:39:32 +0300

    moved to src folder

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import UserContext from "./userContext";
+import UserContext from "./components/UserContext";
 
 function UserDetails() {
   const userData = useContext(UserContext);
