95f5b0010e85b24cb2dbe855508caf9f684904c0
Bino <you@example.comclear>
2024-08-18 21:02:00 +0300

    counter component added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -37,13 +38,10 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile 
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-        />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
