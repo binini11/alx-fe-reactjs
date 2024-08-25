16010f1f97c31f8a0fd00f44ce6173f233c75199
Bino <you@example.comclear>
2024-08-25 10:16:25 +0300

    update and delete components added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   const [currentRecipe, setCurrentRecipe] = useState(null);
@@ -10,14 +12,26 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm
-        currentRecipe={currentRecipe}
-        setCurrentRecipe={setCurrentRecipe}
-      />
-      <RecipeList onEdit={handleEdit} />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm
+                  currentRecipe={currentRecipe}
+                  setCurrentRecipe={setCurrentRecipe}
+                />
+                <RecipeList onEdit={handleEdit} />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
