927d756050691bb7f7e677788ceceb114ffd5e04
Bino <you@example.comclear>
2024-08-25 08:39:39 +0300

    recipe sharing runction complete

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,31 +1,13 @@
-import React, { useState } from "react";
-import useRecipeStore from "./store";
+import React from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
-  const [newRecipe, setNewRecipe] = useState("");
-  const recipes = useRecipeStore((state) => state.recipes);
-  const addRecipe = useRecipeStore((state) => state.addRecipe);
-
-  const handleAddRecipe = () => {
-    addRecipe(newRecipe);
-    setNewRecipe("");
-  };
-
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <input
-        type="text"
-        value={newRecipe}
-        onChange={(e) => setNewRecipe(e.target.value)}
-        placeholder="Add a new recipe"
-      />
-      <button onClick={handleAddRecipe}>Add Recipe</button>
-      <ul>
-        {recipes.map((recipe, index) => (
-          <li key={index}>{recipe}</li>
-        ))}
-      </ul>
+      <AddRecipeForm />
+      <RecipeList />
     </div>
   );
 }
