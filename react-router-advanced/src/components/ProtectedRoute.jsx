62e48b5242fc9be00573b278ca3742361882be1b
binini11 <biniam.y.yohannes@gmail.com>
2024-09-01 22:13:02 +0300

    useauth hook

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
-const ProtectedRoute = ({ isAuthenticated, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth();
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
