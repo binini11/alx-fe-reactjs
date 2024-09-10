ae0ce101dbfcd3a8e6812a467b7ad84b0718ae19
binini11 <biniam.y.yohannes@gmail.com>
2024-09-10 22:50:55 +0300

    recipe with details

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './components/HomePage'
+// src/App.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
-  return(
-    <HomePage />
-  )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
