d4231defa62968b9d07538dff2fb282d638d107b
binini11 <biniam.y.yohannes@gmail.com>
2025-01-14 08:32:08 +0300

    components removed

--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ padding: "10px", margin: "10px" }}>
-      <p style={{ fontSize: "italic" }}>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
