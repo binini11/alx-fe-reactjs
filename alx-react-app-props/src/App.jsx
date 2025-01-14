fccdb5002a78ec85c3f78bf44ab88313143e4dc4
binini11 <biniam.y.yohannes@gmail.com>
2025-01-14 08:49:32 +0300

    all component

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import ProfilePage from "./ProfilePage";
-import UserContext from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
 
-function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+function App(){
+  const userData={name:"Jane Doe", email:"jane.doe@example.com"}
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />
 }
 
-export default App;
+export default App
